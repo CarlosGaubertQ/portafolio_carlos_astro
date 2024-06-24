1859587d0bbf47e30239e96d202238f487e89389
CarlosGaubertQ <carlosgaubertquijada@gmail.com>
2024-06-24 04:22:41 -0400

    Agregando repositorios, arreglando contacto

---
+++ src/lib/data.ts
@@ -0,0 +1,44 @@
+export const repositories = [
+  {
+    patchImage: "/repositories/android.jpeg",
+    nameProject: "Ordenes de trabajo.",
+    descriptionProject:
+      "Sistema de gestión de ordenes de trabajo para la gestión de proyectos. Desarrollado en android utilizando java y REST API usando retrofit.",
+    githubUrl: "https://github.com/CarlosGaubertQ/Ordenes-de-Trabajo-2017",
+  },
+  {
+    patchImage: "/repositories/portafolio-astro.png",
+    nameProject: "Portafolio Carlos Gaubert.",
+    descriptionProject:
+      "Portafolio de proyectos de Carlos Gaubert. Desarrollado con astro y tailwindcss.",
+    githubUrl: "https://github.com/CarlosGaubertQ/portafolio_carlos_astro",
+  },
+  {
+    patchImage: "/repositories/portafolio-astro.png",
+    nameProject: "Sistema de camiones (Frontend).",
+    descriptionProject:
+      "Sistema de gestión de camiones. Este sistema fue desarrollado para reemplazar sistema obsoleto. Desarrollado en ReactJS utilizando material UI.",
+    githubUrl: "https://github.com/CarlosGaubertQ/CamionesUBB",
+  },
+  {
+    patchImage: "/repositories/portafolio-astro.png",
+    nameProject: "Sistema de camiones (Backend).",
+    descriptionProject:
+      "Sistema de gestión de camiones. Este sistema fue desarrollado para reemplazar sistema obsoleto. Desarrollado en NodeJs. Sequelize como ORM.",
+    githubUrl: "https://github.com/CarlosGaubertQ/ServidorCamionesUBB/",
+  },
+  {
+    patchImage: "/repositories/portafolio-astro.png",
+    nameProject: "Calibración imagenes satelitales.",
+    descriptionProject:
+      "Algoritmo de calibración de imagenes satelitales, permite ver comportamiento de modelos VIT. Desarrollado en Python utilizando OpenCV.",
+    githubUrl: "https://github.com/CarlosGaubertQ/calibracion-image-satellite",
+  },
+  {
+    patchImage: "/repositories/portafolio-astro.png",
+    nameProject: "Sistema de clasificación de imágenes VIT.",
+    descriptionProject:
+      "Sistema que permite ver el comportamiento de una zona geografica y calcula el porcentaje de bosque en esta. Esta desarrollado con Python usando Django y utiliza Pytorch para los modelos de inteligencia artificial.",
+    githubUrl: "https://github.com/CarlosGaubertQ/TesisGaubertMapas",
+  },
+];
