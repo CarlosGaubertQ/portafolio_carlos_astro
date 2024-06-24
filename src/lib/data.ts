281810b1e5034b30d1d18c13649fe9b1856ee5ea
CarlosGaubertQ <carlosgaubertquijada@gmail.com>
2024-06-24 05:56:22 -0400

    Agregando imagenes y arreglando hipervinculos

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,44 +1,51 @@
 export const repositories = [
   {
-    patchImage: "/repositories/android.jpeg",
+    patchImage: "/repositories/pytorch.png",
+    nameProject: "Sistema de clasificación de imágenes VIT.",
+    descriptionProject:
+      "Sistema que permite ver el comportamiento de una zona geografica y calcula el porcentaje de bosque en esta. Esta desarrollado con Python usando Django y utiliza Pytorch para los modelos de inteligencia artificial.",
+    githubUrl: "https://github.com/CarlosGaubertQ/TesisGaubertMapas",
+  },
+  {
+    patchImage: "/repositories/javaandroid.jpeg",
     nameProject: "Ordenes de trabajo.",
     descriptionProject:
       "Sistema de gestión de ordenes de trabajo para la gestión de proyectos. Desarrollado en android utilizando java y REST API usando retrofit.",
     githubUrl: "https://github.com/CarlosGaubertQ/Ordenes-de-Trabajo-2017",
   },
   {
-    patchImage: "/repositories/portafolio-astro.png",
+    patchImage: "/repositories/astro.webp",
     nameProject: "Portafolio Carlos Gaubert.",
     descriptionProject:
       "Portafolio de proyectos de Carlos Gaubert. Desarrollado con astro y tailwindcss.",
     githubUrl: "https://github.com/CarlosGaubertQ/portafolio_carlos_astro",
   },
   {
-    patchImage: "/repositories/portafolio-astro.png",
+    patchImage: "/repositories/react.png",
     nameProject: "Sistema de camiones (Frontend).",
     descriptionProject:
       "Sistema de gestión de camiones. Este sistema fue desarrollado para reemplazar sistema obsoleto. Desarrollado en ReactJS utilizando material UI.",
     githubUrl: "https://github.com/CarlosGaubertQ/CamionesUBB",
   },
   {
-    patchImage: "/repositories/portafolio-astro.png",
+    patchImage: "/repositories/node.png",
     nameProject: "Sistema de camiones (Backend).",
     descriptionProject:
       "Sistema de gestión de camiones. Este sistema fue desarrollado para reemplazar sistema obsoleto. Desarrollado en NodeJs. Sequelize como ORM.",
     githubUrl: "https://github.com/CarlosGaubertQ/ServidorCamionesUBB/",
   },
   {
-    patchImage: "/repositories/portafolio-astro.png",
+    patchImage: "/repositories/opencv.png",
     nameProject: "Calibración imagenes satelitales.",
     descriptionProject:
       "Algoritmo de calibración de imagenes satelitales, permite ver comportamiento de modelos VIT. Desarrollado en Python utilizando OpenCV.",
     githubUrl: "https://github.com/CarlosGaubertQ/calibracion-image-satellite",
   },
   {
-    patchImage: "/repositories/portafolio-astro.png",
-    nameProject: "Sistema de clasificación de imágenes VIT.",
+    patchImage: "/repositories/pytorch.png",
+    nameProject: "Entrenamiento de modelos satelitales.",
     descriptionProject:
-      "Sistema que permite ver el comportamiento de una zona geografica y calcula el porcentaje de bosque en esta. Esta desarrollado con Python usando Django y utiliza Pytorch para los modelos de inteligencia artificial.",
-    githubUrl: "https://github.com/CarlosGaubertQ/TesisGaubertMapas",
+      "Sistema de entrenamiento de modelos satelitales. Desarrollado en Python utilizando Pytorch.",
+    githubUrl: "https://github.com/CarlosGaubertQ/ExperimentoModelosSatellite",
   },
 ];
