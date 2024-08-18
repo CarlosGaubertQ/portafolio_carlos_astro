401c18948fa3aab356c8e30c5dc568a90d435626
CarlosGaubertQ <carlosgaubertquijada@gmail.com>
2024-08-18 11:46:58 -0400

    Agregando experiencias

---
+++ src/lib/dataExperience.ts
@@ -0,0 +1,35 @@
+export const experiences = [
+  {
+    dateExperience: "Marzo 2023 – Marzo 2024",
+    companyExperience: "Universidad del Bío-Bío",
+    cityCompanyExperience: "Concepción, Chile",
+    titleExperience: "Investigador",
+    descriptionExperience:
+      "Se participo de un proyecto FIC regional, del cual se realizó modelos VIT para clasificación de imágenes satelitales el cual fue hecho en Python usando Pytorch, se empleó un pequeño sistema para visualizar el comportamiento de los modelos utilizando Django.",
+  },
+  {
+    dateExperience: "Octubre 2021 – Enero 2022",
+    companyExperience: "IT-CONSULTING SPA",
+    cityCompanyExperience: "Santiago, Chile",
+    titleExperience: "Ingeniero de Software",
+    descriptionExperience:
+      "Se desarrolló y lideró un proyecto de gestión de frutas para una empresa exportadora, implementando un sistema basado en el patrón MVC con JavaScript, PHP y MySQL, optimizando operaciones mediante procedimientos almacenados. Se utilizó Bootstrap para el diseño de la interfaz.",
+  },
+  {
+    dateExperience: "Junio 2020 – Abril 2020",
+    companyExperience: "IT-CONSULTING SPA",
+    cityCompanyExperience: "Santiago, Chile",
+    titleExperience: "Ingeniero de Software",
+    descriptionExperience:
+      "Se desarrollo un sistema de gestión de inventario utilizando JavaScript, PHP y MySQL. Responsable de diseñar e implementar varios módulos CRUD para manejar eficientemente el inventario, utilizando procedimientos almacenados en MySQL para garantizar un rendimiento óptimo y una gestión de datos segura.",
+  },
+
+  {
+    dateExperience: "Junio 2018 – Abril 2018",
+    companyExperience: "Hospital las Higueras",
+    cityCompanyExperience: "Talcahuano, Chile",
+    titleExperience: "Soporte Técnico",
+    descriptionExperience:
+      "Como responsable de soporte de TI en un entorno médico, se desempeñó un papel clave en la gestión de equipos, enrutamiento de direcciones IP e implementación de software especializado para una administración eficiente. Se mantuvo una estrecha interacción con el personal médico y administrativo, facilitando una comunicación efectiva y proporcionando soluciones tecnológicas óptimas para respaldar las operaciones críticas.",
+  },
+];
